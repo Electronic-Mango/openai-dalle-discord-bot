746643aaeb1ff8f2122de222f46cf4853db99a57
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-04-15 22:05:30 +0200

    Update names in "generate" module

--- src/commands/generate.py
+++ src/commands/generate.py
@@ -4,23 +4,23 @@ from lightbulb.commands import MessageCommand
 from command_check import check
 from images import generate_image
 
-ask_plugin = Plugin("ask_plugin")
+generate_plugin = Plugin("generate_plugin")
 
 
-@ask_plugin.command()
+@generate_plugin.command()
 @option("prompt", "Prompt to generate image for", str)
 @add_checks(check)
 @command("generate", "Generate image based on prompt", auto_defer=True)
 @implements(SlashCommand)
-async def ask(context: Context) -> None:
+async def generate(context: Context) -> None:
     await _generate_image(context, context.options.prompt)
 
 
-@ask_plugin.command()
+@generate_plugin.command()
 @add_checks(check)
 @command("generate", "Generate image for given message", auto_defer=True)
 @implements(MessageCommand)
-async def ask_directly(context: Context) -> None:
+async def generate_directly(context: Context) -> None:
     await _generate_image(context, context.options.target.content)
 
 
@@ -30,8 +30,8 @@ async def _generate_image(context: Context, prompt: str) -> None:
 
 
 def load(bot: BotApp) -> None:
-    bot.add_plugin(ask_plugin)
+    bot.add_plugin(generate_plugin)
 
 
 def unload(bot: BotApp) -> None:
-    bot.remove_plugin(ask_plugin)
+    bot.remove_plugin(generate_plugin)

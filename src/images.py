38cb8cf7b65afaa5dab8b96573c44efd78cd40fd
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-11-16 12:03:36 +0100

    Improve error handling
    Return only error message, rather than entire error body.

--- src/images.py
+++ src/images.py
@@ -1,9 +1,11 @@
 from os import getenv
 
 from dotenv import load_dotenv
-from openai import OpenAI, OpenAIError
+from openai import APIError, OpenAI, OpenAIError
 
 load_dotenv()
+
+DEFAULT_ERROR_MESSAGE = "Error occurred during image generation"
 
 TOKEN = getenv("OPENAI_TOKEN")
 MODEL = getenv("OPENAI_MODEL")
@@ -22,5 +24,11 @@ def generate_image(prompt: str) -> str:
             n=1,
         )
         return response.data[0].url
-    except OpenAIError as error:
-        return str(error)
+    except APIError as error:
+        return (
+            error.body.get("message", DEFAULT_ERROR_MESSAGE)
+            if isinstance(error.body, dict)
+            else DEFAULT_ERROR_MESSAGE
+        )
+    except OpenAIError:
+        return DEFAULT_ERROR_MESSAGE
